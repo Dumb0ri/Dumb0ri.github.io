7545a8d013a5c7d6f8b4c51fd8d6cd77e6ba4561
DumbOri <112170490+Dumb0ri@users.noreply.github.com>
2023-03-21 22:25:34 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const observer = new IntersectionObserver((e) => {
+    e.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenelements = document.querySelectorAll(".skillHidden");
+hiddenelements.forEach((el) => observer.observe(el));
+
+function easteregg() {
+    alert("🎉축하드립니다 당신은 이스터에그를 발견하셨습니다!!");
+}
+function discordLink() {
+    var discord = "https://discord.com/users/958582255159214132";
+    window.open(discord);
+}
+
+const mobile_nav = document.querySelector(".mobile_nav_icon");
+mobile_nav.onclick = function() {
+    navBar = document.querySelector(".nav_links");
+    navBar.classList.toggle("mobile")
+}
